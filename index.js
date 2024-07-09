4e5724aa37ffa85bd32d00ddb0d32ad51c2956c9
Yuriy Zhvirblis <yuriyzdev@gmail.com>
2024-07-09 20:39:32 +0300

    basic done

--- index.js
+++ index.js
@@ -44,8 +44,59 @@ function updateGrid() {
   });
 }
 
+function nextGeneration() {
+  const newGrid = grid.map((row, rowIndex) =>
+    row.map((cell, colIndex) => {
+      const aliveNeighbors = countAliveNeighbors(rowIndex, colIndex);
+      if (cell && (aliveNeighbors === 2 || aliveNeighbors === 3)) {
+        return true;
+      } else if (!cell && aliveNeighbors === 3) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+  );
+  grid = newGrid;
+  updateGrid();
+}
+
+function countAliveNeighbors(row, col) {
+  let count = 0;
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      if (i === 0 && j === 0) continue;
+      const newRow = row + i;
+      const newCol = col + j;
+      if (
+        newRow >= 0 &&
+        newRow < gridSize &&
+        newCol >= 0 &&
+        newCol < gridSize &&
+        grid[newRow][newCol]
+      ) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+startButton.addEventListener("click", () => {
+  clearInterval(interval);
+  interval = setInterval(nextGeneration, 100);
+});
+
+stopButton.addEventListener("click", () => {
+  clearInterval(interval);
+});
+
 resetButton.addEventListener("click", () => {
   createGrid(gridSize);
+});
+
+stepButton.addEventListener("click", () => {
+  nextGeneration();
 });
 
 gridSizeInput.addEventListener("change", (event) => {
