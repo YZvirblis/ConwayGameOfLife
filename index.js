50202592c58d8efce09e096d95901f78302a9deb
Yuriy Zhvirblis <yuriyzdev@gmail.com>
2024-07-09 18:50:36 +0300

    cells

--- index.js
+++ index.js
@@ -12,13 +12,36 @@ function createGrid(size) {
   gridElement.innerHTML = "";
   gridElement.style.gridTemplateColumns = `repeat(${size}, 20px)`;
   gridElement.style.gridTemplateRows = `repeat(${size}, 20px)`;
+  grid = [];
   for (let row = 0; row < size; row++) {
+    const rowArr = [];
     for (let col = 0; col < size; col++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
+      cell.addEventListener("click", () => toggleCellState(row, col));
       gridElement.appendChild(cell);
+      rowArr.push(false);
     }
+    grid.push(rowArr);
   }
+}
+
+function toggleCellState(row, col) {
+  grid[row][col] = !grid[row][col];
+  updateGrid();
+}
+
+function updateGrid() {
+  grid.forEach((row, rowIndex) => {
+    row.forEach((cell, colIndex) => {
+      const cellElement = gridElement.children[rowIndex * gridSize + colIndex];
+      if (cell) {
+        cellElement.classList.add("alive");
+      } else {
+        cellElement.classList.remove("alive");
+      }
+    });
+  });
 }
 
 gridSizeInput.addEventListener("change", (event) => {
